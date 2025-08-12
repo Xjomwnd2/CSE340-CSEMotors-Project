01b02aff16d4931ab742d86ba01b765d2d6f9daf
YourGitHubUsername <jmwaura@byupathway.edu>
2025-08-13 00:43:33 +0300

    commit

---
+++ controllers/baseController.js
@@ -0,0 +1,13 @@
+const utilities = require("../utilities/")
+const baseController = {}
+
+/* ************************
+ * Build the home view
+ * ************************ */
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  //req.flash("notice", "This is a flash message.")
+  res.render("index", {title: "Home", nav})
+}
+
+module.exports = baseController
