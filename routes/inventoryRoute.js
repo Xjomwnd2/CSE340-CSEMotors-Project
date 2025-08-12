6547ebf6f6f77e265a1ac8c663e082cb43144e2d
YourGitHubUsername <jmwaura@byupathway.edu>
2025-08-13 02:44:00 +0300

    commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,21 +1,21 @@
 // Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/inventoryController")
-const utilities = require("../utilities/")
-const regValidate = require('../utilities/inventory-validation')
+const express = require("express");
+const router = new express.Router(); 
+const invController = require("../controllers/inventoryController");
+const utilities = require("../utilities/");
+const regValidate = require('../utilities/inventory-validation');
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 // Route to build inventory item detail view
-router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInvId))
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInvId));
 
 // Route to build management view
-router.get("/", utilities.checkLogin, utilities.handleErrors(invController.buildManagement))
+router.get("/", utilities.checkLogin, utilities.handleErrors(invController.buildManagement));
 
 // Route to build add classification view
-router.get("/add-classification", utilities.checkLogin, utilities.handleErrors(invController.buildAddClassification))
+router.get("/add-classification", utilities.checkLogin, utilities.handleErrors(invController.buildAddClassification));
 
 // Process the add classification
 router.post(
@@ -23,10 +23,10 @@ router.post(
     regValidate.classificationRules(),
     regValidate.checkClassificationData,
     utilities.handleErrors(invController.addClassification)
-)
+);
 
 // Route to build add inventory view
-router.get("/add-inventory", utilities.checkLogin, utilities.handleErrors(invController.buildAddInventory))
+router.get("/add-inventory", utilities.checkLogin, utilities.handleErrors(invController.buildAddInventory));
 
 // Process the add inventory
 router.post(
@@ -34,9 +34,9 @@ router.post(
     regValidate.inventoryRules(),
     regValidate.checkInventoryData,
     utilities.handleErrors(invController.addInventory)
-)
+);
 
 // Route to get inventory for AJAX request
-router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON));
 
-module.exports = router
+module.exports = router;
