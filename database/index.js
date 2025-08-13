5707f806c3ff5e18c763cc8fcf20d14772a1e626
YourGitHubUsername <jmwaura@byupathway.edu>
2025-08-13 15:44:24 +0300

    commit

--- database/index.js
+++ database/index.js
@@ -4,8 +4,8 @@
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *********************************************** */
-const { Pool } = require("pg")
-require("dotenv").config()
+const { Pool } = require("pg");
+require("dotenv").config();
 
 /* ***********************************************
  * Connection Pool
@@ -20,25 +20,25 @@ if (process.env.NODE_ENV == "development") {
     ssl: {
       rejectUnauthorized: false,
     },
-  })
+  });
 
   // Added for troubleshooting queries
   // during development
   module.exports = {
     async query(text, params) {
       try {
-        const res = await pool.query(text, params)
-        console.log("executed query", { text })
+        const res = await pool.query(text, params);
+        console.log("executed query", { text });
         return res
       } catch (error) {
-        console.error("error in query", { text })
+        console.error("error in query", { text });
         throw error
-      }
+      };
     },
   }
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-  })
+  });
   module.exports = pool
-}
+};
